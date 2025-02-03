17d59b17b792bb432a6346f74af427e93bba10e9
MarucoMass <mariomass23@gmail.com>
2025-02-03 17:04:55 -0300

    added users routes and controllers with its middleware

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ dotenv.config();
 
 import express from "express";
 import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
+import { authenticateToken } from "./middleware/authTokenMiddleware";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/users", authenticateToken, userRoutes)
 
 export default app;
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,12 +1,21 @@
 import { Request, Response } from "express";
 import prisma from "../models/user";
-import { hashPassword } from "../services/passwordService";
+import { hashPassword, comparePasswords } from "../services/passwordService";
 import { generateToken } from "../services/authService";
 
 export const register = async (req:Request, res: Response): Promise<void> => {
     const {email, password} = req.body;
 
     try {
+
+        if (!email) {
+            res.status(400).json({ message: 'Please enter an email' })
+            return
+        }
+        if (!password) {
+            res.status(400).json({ message: 'Please enter a password' })
+            return
+        }
         const hashedPassword = await hashPassword(password);
 
         const user = await prisma.create({
@@ -18,8 +27,48 @@ export const register = async (req:Request, res: Response): Promise<void> => {
 
         const token = generateToken(user);
         res.status(201).json({token});
-    } catch (error) {
+    } catch (error: any) {
+
+        if (error?.code === 'P2002' && error?.meta?.target?.includes('email')) {
+            res.status(400).json({ message: 'The email has been already registered' })
+        }
         console.log(error);
         res.status(500).json({error: "There's an error"});
     }
+}
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+
+    const { email, password } = req.body
+
+    try {
+
+        if (!email) {
+            res.status(400).json({ message: 'Please enter an email' })
+            return
+        }
+        if (!password) {
+            res.status(400).json({ message: 'Please enter a password' })
+            return
+        }
+
+        const user = await prisma.findUnique({ where: { email } })
+        if (!user) {
+            res.status(404).json({ error: 'User not found' })
+            return
+        }
+
+        const passwordMatch = await comparePasswords(password, user.password);
+        if (!passwordMatch) {
+            res.status(401).json({ error: "User and password doesn't match" })
+        }
+
+        const token = generateToken(user)
+        res.status(200).json({ token })
+
+
+    } catch (error: any) {
+        console.log('Error: ', error)
+    }
+
 }
